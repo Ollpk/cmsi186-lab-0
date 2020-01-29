8aff122a7cc4fedbb7d82f7d9c912309db40f3f9
Jake Griffin <jgriff25@lion.lmu.edu>
2020-01-28 16:36:16 -0800

    Greeter.java

--- Greeter.java
+++ Greeter.java
@@ -1,5 +1,5 @@
 public class Greeter {
     public static void main(String args[]) {
-        System.out.println("His this is Jake after I have updated my file as of 1.28.2020!!");
+        System.out.println("His this is Jacob Griffin after I have updated my file as of 1.28.2020!!");
     }
 }
